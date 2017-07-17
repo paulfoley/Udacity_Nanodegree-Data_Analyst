19e09f1d703f5a30c3eb94e39e31cd2ca3c8b22d
Paul Foley <me@paulnicholasfoley.com>
2017-07-17 16:07:11 -0600

    Update Numpy Playground
    Add in code for comments for readability. Also, added a README.md for easy to read documentation

---
+++ Numpy-Playground/README.md
@@ -0,0 +1,48 @@
+# Playground - Numpy
+
+[Numpy](http://www.numpy.org/) is a library which provides functions that are especially useful when you have to work with large arrays and matrices of numeric data, like doing matrix matrix multiplications and dot products. 
+
+Also, [Numpy](http://www.numpy.org/) is battle tested and optimized, so that it runs fast, much faster than if you were working with Python lists directly.
+
+
+## Project Overview
+
+In this project, we'll play with the Numpy library, specifically:
+
+* Arrays
+* Arithmetic Operations
+* Mathematical Operations
+
+
+## Getting Started
+
+### Prerequisites
+
+You'll need to install:
+
+* [Anaconda](https://www.continuum.io/downloads)
+* [Python (Minimum 3)](https://www.continuum.io/blog/developer-blog/python-3-support-anaconda)
+* [Numpy](http://www.numpy.org/)
+
+
+## Scripts
+
+* `numpy_playground.py` - Script that shows off [Numpy](http://www.numpy.org/)'s various functions.
+
+
+## Authors
+
+* **[Udacity](https://www.udacity.com/)***
+* [Paul Foley](https://github.com/paulfoley)
+
+* Other [contributors](https://github.com/paulfoley/data-analyst/tree/master/ERCOT-Parse) who participated in this project.
+
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+
+## Acknowledgments
+
+* [Numpy](http://www.numpy.org/)
--- Numpy-Playground/numpy_playground.py
+++ Numpy-Playground/numpy_playground.py
@@ -1,21 +1,28 @@
 '''
-The following code is to play with Numpy, which is a library 
-that provides functions that are especially useful when you have to
-work with large arrays and matrices of numeric data, like doing 
-matrix matrix multiplications. Also, Numpy is battle tested and 
-optimized so that it runs fast, much faster than if you were working
-with Python lists directly.
+The following code is to play with the Numpy library
+
+Numpy is a library that provides functions that are especially useful 
+when you have to work with large arrays and matrices of numeric data, 
+like doing matrix matrix multiplications. 
+
+Also, Numpy is battle tested and optimized so that it runs fast,
+much faster than if you were working with Python lists directly.
 '''
 
-# Import Numpy
+## Imports
 import numpy as np
 
+## Playground
+
+### Arrays
 '''
-The array object class is the foundation of Numpy, and Numpy arrays are like
-lists in Python, except that every thing inside an array must be of the
-same type, like int or float.
+The array object class is the foundation of Numpy
+
+Numpy arrays are like lists in Python,
+except that every thing inside an array must be of the same type,
+like int, str, or float.
 '''
-# Change False to True to see Numpy arrays in action
+#### Change False to True to see Numpy Arrays in action
 if True:
     array = np.array([1, 4, 5, 8], float)
     print("Numpy Arrays")
@@ -25,10 +32,10 @@ if True:
     print (array)
 
 '''
-You can index, slice, and manipulate a Numpy array much like you would with a
-a Python list.
+You can index, slice, and manipulate a Numpy array 
+much like you would with a Python list.
 '''
-# Change False to True to see array indexing and slicing in action
+#### Change False to True to see array indexing and slicing in action
 if True:
     array = np.array([1, 4, 5, 8], float)
     print("")
@@ -42,7 +49,7 @@ if True:
     array[1] = 5.0
     print(array[1])
 
-# Change False to True to see Matrix indexing and slicing in action
+#### Change False to True to see Matrix indexing and slicing in action
 if True:
     two_D_array = np.array([[1, 2, 3], [4, 5, 6]], float)
     print("")
@@ -55,10 +62,12 @@ if True:
     print("")
     print(two_D_array[:, 2])
 
+
+### Arithmetic Operations
 '''
 Here are some arithmetic operations that you can do with Numpy arrays
 '''
-# Change False to True to see Array arithmetics in action
+#### Change False to True to see Array arithmetics in action
 if True:
     array_1 = np.array([1, 2, 3], float)
     array_2 = np.array([5, 2, 6], float)
@@ -70,7 +79,7 @@ if True:
     print("")
     print(array_1 * array_2)
 
-# Change False to True to see Matrix arithmetics in action
+#### Change False to True to see Matrix arithmetics in action
 if True:
     array_1 = np.array([[1, 2], [3, 4]], float)
     array_2 = np.array([[5, 6], [7, 8]], float)
@@ -82,13 +91,15 @@ if True:
     print("")
     print(array_1 * array_2)
 
-'''
-In addition to the standard arthimetic operations, Numpy also has a range of
-other mathematical operations that you can apply to Numpy arrays, such as
-mean and dot product.
 
-Both of these functions will be useful in later programming quizzes.
+### Mathematical Operations
 '''
+In addition to the standard arthimetic operations,
+Numpy also has a range of other mathematical operations 
+that you can apply to Numpy arrays, 
+such as mean and dot product.
+'''
+#### Change False to True to see Mathematical Operations in action
 if True:
     array_1 = np.array([1, 2, 3], float)
     array_2 = np.array([[6], [7], [8]], float)
