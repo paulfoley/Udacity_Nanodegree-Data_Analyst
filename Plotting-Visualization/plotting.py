26bd401c123729450a17c900af0cfa23c3262bd8
Paul Foley <me@paulnicholasfoley.com>
2017-07-17 20:20:21 -0600

    Delete Patent and Plotting Projects

--- Plotting-Visualization/plotting.py
+++
@@ -1,5 +0,0 @@
-from ggplot import *
-from pandas import *
-
-hr_year = pandas.read_csv(put_csv_here)
-print(ggplot(hr_year, aes('yearID', 'HR')) + geom_point(color = 'red') + geom_line(color = 'red') + ggtitle('Total HRs By Year') + xlab('Year') + ylab('HR'))
